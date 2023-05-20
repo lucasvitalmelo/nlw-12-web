ade0594a3cd51a8c7f6efec9fd4aa953eccb51e9
Lucas Vital de Melo <lucasvitaldemelo@gmail.com>
2023-05-20 14:56:12 -0300

    aula 4

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -16,7 +16,7 @@ export function Profile() {
       <p className="max-w-[140px] text-sm leading-snug">
         {name}
         <a
-          href=""
+          href="/api/auth/logout"
           className="block text-red-300 transition-colors hover:text-red-400"
         >
           Quero sair
